516eaab50dfc16ba2f7f57dced638577716cf495
keincarrillo <kein.carrillo.escom@gmail.com>
2026-02-17 14:12:30 -0600

    fix: update header layout and title casing in DashboardLayout for consistency

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,8 +7,8 @@ interface Props {
 const DashboardLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen bg-main text-font-primary p-6 space-y-6">
-      <header className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">MOLINO 1 - MX</h1>
+      <header className="md:flex md:justify-between md:items-center">
+        <h1 className="text-xl font-bold uppercase">molino 1 - mx</h1>
         <span className="text-md opacity-80">
           <Clock />
         </span>
