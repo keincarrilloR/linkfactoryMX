20ac2dcbda3e734f05080c2fdbba87fb238d76ae
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-17 08:44:07 -0600

    refactor: remove unused molinoData and adjust connection status display

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useSocket } from './hooks/useSocket'
 import DashboardLayout from './layout/Layout'
 import Informativo from './components/main/Informativo'
 import Variables from './components/main/Variables'
+import { CircleCheck, CircleX } from 'lucide-react'
 
 const App = () => {
-  const { molinoData, isConnected } = useSocket()
+  const { isConnected } = useSocket()
 
   return (
     <DashboardLayout>
@@ -12,9 +13,13 @@ const App = () => {
         <Informativo />
         <Variables />
         <div className="mt-4 flex flex-col sm:flex-row justify-between items-center text-sm opacity-70 gap-2">
-          <span className="flex items-center gap-2">
+          <span className="flex items-center gap-2 text-md">
             {isConnected ? 'Conectado' : 'Desconectado'}
-            {molinoData && (molinoData.run ? ` |  Operando` : ` |  Detenido`)}
+            {isConnected ? (
+              <CircleCheck className="w-5 h-5 text-on" />
+            ) : (
+              <CircleX className="w-5 h-5 text-off" />
+            )}
           </span>
         </div>
       </div>
