69b105c5a7975e2864667de61cc38308bbeef051
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-23 10:19:34 -0600

    feat: add colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,10 @@
 const colors = {
   green: 'bg-green-600 border-green-500',
   orange: 'bg-orange-500 border-orange-400',
-  blue: 'bg-blue-600 border-blue-500'
+  red: 'bg-red-600 border-red-500',
+  purple: 'bg-purple-600 border-purple-500',
+  stop: 'bg-gray-600 border-gray-500',
+  black: 'bg-black border-gray-800'
 }
 
 export default colors
