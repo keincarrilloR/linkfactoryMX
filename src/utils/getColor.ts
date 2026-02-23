f20786a203ff0567d525de1a8d4aeafa14215db4
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-23 10:58:55 -0600

    fix: problem to colors

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -5,10 +5,10 @@ export const getColor = (min: number, max: number, value: number) => {
 
   if (min === 0 && max === 0) return 'purple'
 
-  if (value < min || value > max) return 'red'
-
-  const range = (max - min) * 0.2
+  const range = (max - min) * 0.1
   if (value <= min + range || value >= max - range) return 'orange'
+
+  if (value < min || value > max) return 'red'
 
   return 'green'
 }
