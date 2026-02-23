6c6ee4e94661de8176e2f3c6e4a46068d3c7ff9b
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-23 10:48:25 -0600

    fix: change logic the colors

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -1,17 +1,14 @@
 export const getColor = (min: number, max: number, value: number) => {
-  if (value < max && value > min) return 'green'
-  // Detenido
-  if (String(value) === '0') return 'stop'
-
-  if (min === 0 && max === 0) return 'purple' // Sin rango
-  // Preventivo
-  if (max - max * 0.35 > value) return 'orange' // Bajo
-  if (min + min * 0.35 < value) return 'orange' // Alto
-
-  // Fuera del rango
-  if (value < min) return 'red'
-  if (value > max) return 'red'
-
-  // Sin conexion o datos invalidos
   if (isNaN(value) || isNaN(min) || isNaN(max)) return 'black'
+
+  if (value === 0) return 'stop'
+
+  if (min === 0 && max === 0) return 'purple'
+
+  if (value < min || value > max) return 'red'
+
+  const range = (max - min) * 0.2
+  if (value <= min + range || value >= max - range) return 'orange'
+
+  return 'green'
 }
