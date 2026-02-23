866335d756dbecd7852e301e614cea0dd788d617
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-23 10:19:49 -0600

    feat: create function getColor

---
+++ src/utils/getColor.ts
@@ -0,0 +1,16 @@
+export const getColor = (min: number, max: number, value: number) => {
+  if (value < max && value > min) return 'green'
+  // Detenido
+  if (String(value) === '0') return 'stop'
+
+  // Preventivo
+  if (max - max * 0.35 > value) return 'orange' // Bajo
+  if (min + min * 0.35 < value) return 'orange' // Alto
+
+  // Fuera del rango
+  if (value < min) return 'red'
+  if (value > max) return 'red'
+
+  // Sin conexion o datos invalidos
+  if (isNaN(value) || isNaN(min) || isNaN(max)) return 'black'
+}
