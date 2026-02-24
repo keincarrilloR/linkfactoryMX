1a95b6b702c61035d6fd49f79131048944b2c1ec
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-02-24 11:40:06 -0600

    fix: cgange of color logic

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -1,7 +1,3 @@
-import { useSocket } from '../hooks/useSocket'
-
-const { molinoData } = useSocket()
-
 const medias = (max: number, min: number, nom: number) => {
   const mediaInferior = (min + nom) / 2
   const mediaSuperior = (max + nom) / 2
@@ -12,15 +8,14 @@ export const getColor = (
   min: number,
   max: number,
   nom: number,
-  value: number
+  value: number,
+  estado?: string
 ) => {
   const { mediaInferior, mediaSuperior } = medias(max, min, nom)
   if (isNaN(value) || isNaN(min) || isNaN(max)) return 'black'
 
-  if (molinoData?.informativo?.estadoYRun?.estadoMol1 !== 'Corriendo')
-    return 'stop'
-
   if (value === 0) return 'stop'
+  if (estado === 'Detenido') return 'stop'
 
   if (min === 0 && max === 0) return 'purple'
 
